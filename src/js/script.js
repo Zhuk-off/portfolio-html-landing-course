f2b759d318d4713cb477b6abfb6a4533fa8693c5
Alexander <zhukoff.alexander@gmail.com>
2022-10-23 22:25:15 +0300

    feat: Create a fourth screen
    Create the fourth landing screen. Add a script for converting knowledge percentages to the load line.

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,23 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+const percents = document.querySelectorAll('.skills__quality-percent');
+const lines = document.querySelectorAll('.skills__quality-graphic-load');
+
+percents.forEach((p, i) => {
+    const percent = Number.parseInt(p.innerHTML);
+    console.log(percent);
+    if (percent < 0) {
+        lines[i].style.width = '0%';
+        return;
+    }
+    if (percent > 100) {
+        lines[i].style.width = '100%';
+        return;
+    }
+    if (!Number.isNaN(percent)) {
+        lines[i].style.width = p.innerHTML;
+    } else {
+        lines[i].style.width = '0%';
+    }
+});
